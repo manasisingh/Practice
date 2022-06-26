a1bc674464b1f11a8f883dd74957bae1716ea872
manasisingh <manasi.singh@extwebtech.in>
2022-06-27 01:41:45 +0530

    changes reverted

--- src/Components/CompB.js
+++ src/Components/CompB.js
@@ -4,8 +4,11 @@ const CompB = () => {
     return(
         <div>
             I am Comp B from manasi-dev1 branch
+<<<<<<< HEAD
             <p>I am paragraph from manasi-dev1 branch</p>
             <p>I am paragraph from manasi-dev branch</p>
+=======
+>>>>>>> parent of 0dc6155 (paragraph added in Comp B of manasi-dev1)
         </div>
     )
 }
